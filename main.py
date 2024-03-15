4256ea33b05328a84f8e7cd9c7ae051293949a11
LAPTOP-GHTIOM0K\umerf <umer.faruque87@gmail.com>
2024-03-16 04:13:12 +0530

    port added to main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 # Mount routers
 app.include_router(authapi.router)
 app.include_router(mainapi.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    # uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="localhost", port=8001)
